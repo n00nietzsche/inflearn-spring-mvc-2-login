0a827fe6b205458e97adbebbf276291cb6d14c8f
n00nietzsche <n00nietzsche@gmail.com>
2021-08-29 09:11:36 +0900

    스프링 MVC 2편 - 로그인 처리-1 쿠키, 세션 #5 로그인/로그아웃 처리하기 - 쿠키 사용

--- src/main/java/hello/login/domain/login/LoginController.java
+++ src/main/java/hello/login/domain/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -24,7 +26,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         // 일반 검증 에러 발생시
         if(bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -41,7 +43,23 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // TODO: 로그인 성공 처리
+        // 로그인 성공 후 쿠키 추가
+        // 시간 정보를 주지 않으면 세션 쿠키 (브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,42 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    // @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    // 실제 memberId 의 쿠키 값은 문자열이지만, 자동 타입 컨버팅이 되어서 Long 형태로 받을 수 있다.
+    // 비로그인 사용자 고려하기 위해 required는 false로 설정해두었다.
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if(memberId == null) {
+            return "home";
+        }
+
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+
+        if(loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
